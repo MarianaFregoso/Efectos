a2e7761744a8d95fe6dd9a56291a3897db1be040
Usuario <Usuario@DESKTOP-0S8D9QP>
2018-03-01 18:40:19 +0100

    Agregar archivos de proyecto.

---
+++ Efectos/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Win32;
+using NAudio;
+using NAudio.Wave;
+
+namespace Efectos
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        WaveOutEvent waveout;
+        AudioFileReader reader;
+        efectos efectoProvider;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            waveout = new WaveOutEvent();
+        }
+
+        private void btnexaminar_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+             if ((bool)fileDialog.ShowDialog())
+            {
+                txtruta.Text = fileDialog.FileName;
+                reader = new AudioFileReader(fileDialog.FileName);
+            }
+
+        }
+
+        private void btnrepro_Click(object sender, RoutedEventArgs e)
+        {
+            if (waveout !=null)
+            {
+                if (waveout.PlaybackState == PlaybackState.Playing)
+                {
+                    waveout.Stop();
+                }
+                waveout.Init(reader);
+                
+                waveout.Play();
+            }
+            
+        }
+
+        private void btnefect_Click(object sender, RoutedEventArgs e)
+        {
+            if (waveout != null && reader != null)
+            {
+                if (waveout.PlaybackState == PlaybackState.Playing)
+                {
+                    waveout.Stop();
+                }
+                efectoProvider = new efectos(reader);
+                waveout.Init(efectoProvider);
+                waveout.Play();
+            }
+        }
+    }
+}
---
+++ Efectos/efectos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace Efectos
+{
+    class efectos : ISampleProvider
+    {
+
+        private ISampleProvider fuente;
+        public efectos (ISampleProvider fuente)
+        {
+            this.fuente = fuente;
+        }
+        public WaveFormat WaveFormat => throw new NotImplementedException();
+
+        public int Read(float[] buffer, int offset, int count)
+        {
+            var read = fuente.Read(buffer, offset, count);
+
+            for (int i=0; i < read; i++)
+            {
+                //efecto
+                buffer[offset + i] *= 0.5f;
+            }
+
+            return read;
+        }
+    }
+}
